5a2885ba3d170550f50bba454eb7bd7a8192b7c0
student <msmrkovic1@etf.unsa.ba>
2019-03-25 11:59:25 +0100

    dodaj metodu sumaCifara

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    public TextField ulaz;
+    public Label ispisBrojeva;
+
+    public void ispisBrojeva(ActionEvent actionEvent) {
+        int n = Integer.parseInt(ulaz.getText());
+
+    String rez = "";
+
+    for (int i=1; i<=n; i++) {
+        if (i % sumaCifara(i) == 0) {
+            rez+=i + " ";
+        }
+    }
+    ispisBrojeva.setText(rez);
+    }
+
+    private static int sumaCifara(int n) {
+        int sum=0;
+
+        while(n!=0) {
+            sum+=n%10;
+            n=n/10;
+        }
+
+        return sum;
+    }
 }
